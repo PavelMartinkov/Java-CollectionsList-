9c7ff4e2d8d931b50c183bc70080b46def42bfc2
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-05-28 15:42:38 +0300

    todo

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,36 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
+        String ADD = "ADD";
+        String EDIT = "EDIT";
+        String DELETE = "DELETE";
+        String LIST = "LIST";
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+                if (input.isEmpty()) {
+                    scanner.close();
+                    break;
+                } else if (input.contains(ADD)) {
+                    todoList.add(input.substring(4));
+                } else if (input.contains(EDIT)) {
+                    todoList.edit(0,"");
+                    continue;
+                } else if (input.contains(DELETE)) {
+                    todoList.delete(0);
+                    continue;
+                } else if (input.contains(LIST)) {
+                    todoList.getTodos();
+                    continue;
+                }
+            }
         // TODO: написать консольное приложение для работы со списком дел todoList
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -4,28 +4,54 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> deal = new ArrayList<>();
+
     public void add(String todo) {
+        deal.add(todo);
+        System.out.println("Добавлено дело " + todo);
         // TODO: добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
+        if (index >= deal.size()) {
+            deal.add(todo);
+            System.out.println("Дело с таким номером не существует");
+        } else {
+            deal.add(index,todo);
+            System.out.println("Добавлено дело " + todo);
+        }
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
     }
 
     public void edit(int index, String todo) {
+        if (index >= deal.size()) {
+            System.out.println("Невозможно заменить дело");
+        } else {
+            deal.set(index,todo);
+            System.out.println("Дело " + deal.get(index) + " заменено на " + todo);
+        }
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
     }
 
     public void delete(int index) {
+        if (index >= deal.size()) {
+            System.out.println("Дело с таким номером не существует");
+        } else {
+            deal.remove(index);
+            System.out.println("Дело " + deal.get(index) + " удалено");
+        }
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
     }
 
     public ArrayList<String> getTodos() {
+        for (int i = 0; i < deal.size(); i++) {
+            System.out.println(i + " - " + deal.get(i));
+        }
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return deal;
     }
 
 }
